c22f0753add0775869b5b73375473292c6c5b44a
aleded32 <alededwards32@gmail.com>
2020-11-13 15:50:19 +0000

    v1.2
    added in shader for pixelating assets within the camera. To make it look like a ps1 game.

--- Assets/scripts/MeshGenerator.cs
+++ Assets/scripts/MeshGenerator.cs
@@ -9,30 +9,38 @@ public class MeshGenerator
 
     public MeshData GenerateTerrainMesh(float[,] MapSize, float randMult)
     {
+        //gets the width and the height of the map.
         int width = MapSize.GetLength(0); // x
         int height = MapSize.GetLength(1); // z
         
 
 
-
+        //calls a new meshData to be used to generate the mesh to map.
         MeshData meshData = new MeshData(width, height);
+
         int vertexIndex = 0;
 
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
+                //calculates the random use of perlin noise to generate slight hills
                 noise = Mathf.PerlinNoise(j * randMult, i * randMult) * 2.0f;
+
+                //sets the vertices to positions int the array, to create a 2D plane.
                 meshData.vertices[vertexIndex] = new Vector3(j, noise, i);
                
-
+                //calculates the terrain within the map size (stops it from generating it outside of scope)
                 if (j < width - 1 && i < height - 1)
                 {
-                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
-                    meshData.AddTriangle(vertexIndex  + width + 1, vertexIndex, vertexIndex + 1);
+                   //adds triangles to each position in the map
+                   meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
+                   meshData.AddTriangle(vertexIndex  + width + 1, vertexIndex, vertexIndex + 1);
+
                 }
 
                 vertexIndex++;
+               
             }
         }
         
@@ -40,37 +48,49 @@ public class MeshGenerator
     }
 }
 
+//holds meshData that allows mesh to be created
 public class MeshData
 {
+    //vertices 
     public Vector3[] vertices;
+
+    //triangle array. keeps count of all the triangles created.
     public int[] triangles;
 
-    int triIndex;
+    //index of triangle array
+    public int triIndex;
 
+    //constructor for meshdata class
     public MeshData(int meshWidth, int meshHeight)
     {
+        //maps the vertices array to the size of the map.
         vertices = new Vector3[meshWidth * meshHeight];
-        triangles = new int[(meshWidth - 1) * (meshHeight-1) *6];
+        //maps the triangles to the size of the map.
+        triangles = new int[(meshWidth) * (meshHeight) *6];
     }
 
+    //adds a vertices to calculate a triangle and iterates the next triangle
     public void AddTriangle(int a, int b, int c)
     {
-        triangles[triIndex] = c;
-        triangles[triIndex + 1] = b;
         triangles[triIndex + 2] = a;
+        triangles[triIndex + 1] = b;
+        triangles[triIndex] = c;
 
         triIndex += 3;
 
         
     }
 
+
+    //this creates a mesh to be used for the terrain 
     public Mesh createMesh()
     {
         Mesh mesh = new Mesh();
 
-
+        //sets vertices and trianlges created to the mesh.
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        //recalulates normals of traingles and vertices for ligthing purposes
         mesh.RecalculateNormals();
 
         return mesh;
--- Assets/scripts/drawTerrain.cs
+++ Assets/scripts/drawTerrain.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class drawTerrain : MonoBehaviour
 {
-
+    //creates a meshGenetor so the mesh can be draw.
     MeshGenerator terrain = new MeshGenerator();
+
+    //these are used to draw the mesh on screen.
     public MeshFilter meshFilter;
     public MeshRenderer meshRender;
     public MeshCollider collider;
+
+    //material for terrain
     public Material Floor;
+
+    //random multiplier for perlin noise.
     public float randMultiplier;
+
+    //allows use to call from and create meshData class
     MeshData terrainData;
+    
+    //sets the size of the terrain and number of vertices needed.
     public float[,] Map = new float[60,60];
+
+    
     public GameObject tree;
+
+    //variables used to create random spacing between trees.
     int treeSpacingX;
     int treeSpacingZ;
     Vector3 offset;
+
+    //sets the mesh 
     public Mesh mesh;
   
 
@@ -24,14 +40,22 @@ public class drawTerrain : MonoBehaviour
     public void Start()
     {
         randMultiplier = Random.Range(0.08f, 0.2f);
+
+        //creates a new terrain
         terrainData = terrain.GenerateTerrainMesh(Map, randMultiplier);
+
+        //draws the terrain to the screen
         drawMesh(terrainData);
-        
+
+        //adds trees to the screen
         addTree();
+
+        //creates the mesh of the terrain.
         mesh = terrainData.createMesh();
 
     }
-
+    
+    
     public void drawMesh(MeshData meshdata)
     {
         meshFilter.sharedMesh = meshdata.createMesh();
@@ -39,6 +63,7 @@ public class drawTerrain : MonoBehaviour
         collider.sharedMesh = meshdata.createMesh();
     }
 
+   //creates a list of trees to be spawned in.
     public List<GameObject> addTree()
     {
         List<GameObject> treelist = new List<GameObject>();
@@ -50,7 +75,9 @@ public class drawTerrain : MonoBehaviour
                 treeSpacingZ = Random.Range(5, 8);
                 offset = new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2));
 
+                //make sure the position is set properly for the trees on the terrain
                 Vector3 position = new Vector3(i, terrain.noise - 0.5f, j);
+
                 treelist.Add(Instantiate(tree, position + offset, Quaternion.identity));
             }
         }
